273547ca16a97ed90de8eb9b9ce8b7d1e2dd3f7e
unknown <sabrina.gomes2@estudante.ifms.edu.br>
2023-05-04 16:29:54 -0400

    coreção

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { GridComponent } from './components/grid/grid.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
@@ -8,6 +9,7 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'hero', component: HeroComponent},
+  { path: 'grid', component: GridComponent},
   { path: 'dashboard', component: DashboardComponent},
 
   { path: '', redirectTo: '/login', pathMatch: 'full'},
